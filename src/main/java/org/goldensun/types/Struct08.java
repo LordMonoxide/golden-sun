33fa32b47552357e0465391590ffe3c91ae33c27
corey Frenette <lordmonoxide@gmail.com>
2024-11-03 09:02:00 -0400

    Sorting relies on signed comparison

--- src/main/java/org/goldensun/types/Struct08.java
+++ src/main/java/org/goldensun/types/Struct08.java
@@ -4,21 +4,21 @@ import org.goldensun.memory.Value;
 import org.goldensun.memory.types.MemoryRef;
 import org.goldensun.memory.types.Pointer;
 import org.goldensun.memory.types.RunnableRef;
+import org.goldensun.memory.types.ShortRef;
 import org.goldensun.memory.types.UnsignedByteRef;
-import org.goldensun.memory.types.UnsignedShortRef;
 
 public class Struct08 implements MemoryRef {
   private final Value ref;
 
   public final Pointer<RunnableRef> _00;
-  public final UnsignedShortRef _04;
+  public final ShortRef _04;
   public final UnsignedByteRef _06;
 
   public Struct08(final Value ref) {
     this.ref = ref;
 
     this._00 = ref.offset(4, 0x00).cast(Pointer.deferred(4, RunnableRef::new));
-    this._04 = ref.offset(2, 0x04).cast(UnsignedShortRef::new);
+    this._04 = ref.offset(2, 0x04).cast(ShortRef::new);
     this._06 = ref.offset(1, 0x06).cast(UnsignedByteRef::new);
   }
 
